4432c4eb9c29646c26413e2515d66bcf25f7cded
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-04-01 21:49:11 -0300

    creating User Model

--- AuthCrud/Data/AppDbContext.cs
+++ AuthCrud/Data/AppDbContext.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AuthCrud.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthCrud.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {         
+        {
         }
+        public DbSet<Usuario> Usuarios {get; set;}
     }
 }
---
+++ AuthCrud/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthCrud.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public byte[] SenhaHash { get; set; }
+        public byte[] SenhaSalt { get; set; }
+    }
+}
